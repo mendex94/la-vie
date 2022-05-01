c1fc0de3f8136d1f42ecd1ce13e5636685e797b8
Thais Cavalcante <thaispcavalcante@gmail.com>
2022-05-01 00:04:07 -0300

    CRUD pacientes

--- src/controllers/pacientesController.js
+++ src/controllers/pacientesController.js
@@ -1,48 +1,76 @@
 const Pacientes = require("../models/Pacientes.js");
 
 const PacientesController = {
-    async listarTodos(req, res) {
-        const pacientes = await Pacientes.findAll();
-        res.json(pacientes);
-    },
-    async buscarPorId(req, res) {
-        const { paciente_id } = req.params;
-        const paciente = await Pacientes.findByPk({
-            where: {
-                paciente_id
-            }
-        });
-        res.json(paciente);
-    },
-    async cadastrar(req, res) {
-        const {nome, email, idade} = req.body;
-        const novoPaciente = await Pacientes.create({
-            nome,
-            email,
-            idade
-
-        });
-        res.json(novoPaciente);
-    },
-    async atualizar(req, res) {
-        const {paciente_id} = req.params;
-        const {nome, email, idade} = req.body;
-        await Pacientes.update({
-            nome,
-            email,
-            idade
-
-        }, {
-            where: {
-                paciente_id
-            }
-        });
-        const pacienteAtualizado = await Pacientes.findByPk(paciente_id);
-        res.status(200).json(pacienteAtualizado);
-    },
-    async deletar (req, res) {
-
+  async listarTodos(req, res) {
+    try {
+      const pacientes = await Pacientes.findAll();
+      res.status(200).json(pacientes);
+    } catch (error) {
+      res.status(500).send("Erro ao recuperar dados do banco");
     }
+  },
+  async buscarPorId(req, res) {
+    const { paciente_id } = req.params;
+    try {
+      const paciente = await Pacientes.findByPk(paciente_id);
+      if (paciente) {
+        res.status(200).json(paciente);
+      } else {
+        res.status(404).json("Id não encontrado");
+      }
+    } catch (error) {
+      res.status(500).send("Erro ao recuperar dados do banco");
+    }
+  },
+  async cadastrar(req, res) {
+    const { nome, email, idade } = req.body;
+    const novoPaciente = await Pacientes.create({
+      nome,
+      email,
+      idade,
+    });
+    res.status(201).json(novoPaciente);
+  },
+  async atualizar(req, res) {
+    const { paciente_id } = req.params;
+    const { nome, email, idade } = req.body;
+    try {
+      await Pacientes.update(
+        {
+          nome,
+          email,
+          idade,
+        },
+        {
+          where: {
+            paciente_id,
+          },
+        }
+      );
+      const pacienteAtualizado = await Pacientes.findByPk(paciente_id);
+      res.status(200).json(pacienteAtualizado);
+    } catch (error) {
+      res.status(500).send("Erro ao recuperar dados do banco");
+    }
+  },
+  async deletar(req, res) {
+    const { paciente_id } = req.params;
+    try {
+      const paciente = await Pacientes.findByPk(paciente_id);
+      if (!paciente) {
+        res.status(404).send("Id não encontrado");
+      } else {
+        await Pacientes.destroy({
+          where: {
+            paciente_id,
+          },
+        });
+        res.send("Paciente excluido com sucesso");
+      }
+    } catch (error) {
+      res.status(500).send("Erro ao recuperar dados do banco");
+    }
+  },
 };
 
 module.exports = PacientesController;
--- src/routes/index.js
+++ src/routes/index.js
@@ -5,16 +5,17 @@ const psicologosController = require('../controllers/psicologos.controller')
 const authController = require('../controllers/auth.controller')
 const psicologosCreateValidation = require('../validations/psicologos/create')
 const authLoginValidation = require('../validations/auth/login')
-
+const pacientesCreateValidation = require('../validations/pacientes/create');
+const pacientesUpdateValidation = require('../validations/pacientes/update');
 
 routes.post('/psicologos', psicologosCreateValidation, psicologosController.cadastrarPsico)
 routes.post('/login', authLoginValidation, authController.login)
 
 
 routes.get("/pacientes", PacientesController.listarTodos);
-routes.get("/pacientes/:paciente_id", PacientesController.listarTodos);
-routes.post("/pacientes", PacientesController.cadastrar);
-routes.put("/pacientes/:paciente_id", PacientesController.atualizar);
+routes.get("/pacientes/:paciente_id", PacientesController.buscarPorId);
+routes.post("/pacientes", pacientesCreateValidation, PacientesController.cadastrar);
+routes.put("/pacientes/:paciente_id", pacientesUpdateValidation, PacientesController.atualizar);
 routes.delete("/pacientes/:paciente_id", PacientesController.deletar);
 
 module.exports = routes
