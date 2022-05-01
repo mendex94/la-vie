d6b2d8d9442c5a5b8c1d283ee2d8dbe3f40d42bf
Felipe Mendes <lfelipecmendes@icloud.com>
2022-05-01 08:33:27 -0300

    minor changes

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -2,5 +2,6 @@ const expressJWT = require('express-jwt')
 const secret = require('../config/secret')
 
 module.exports = expressJWT({
-    secret: secret.key
+    secret: secret.key,
+    algorithms: ["HS256"],
 })
