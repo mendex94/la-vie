17cec0cdadec9205040f3840b10dbb60c3ca3bf8
Felipe Mendes <lfelipecmendes@icloud.com>
2022-05-03 21:57:09 -0300

    projeto finalizado

--- src/controllers/dashboardController.js
+++ src/controllers/dashboardController.js
@@ -1,32 +1,29 @@
 const { Atendimentos, Psicologos, Pacientes } = require('../models');
 
 const dashboardController = {
-    async numeroDeAtendimentos(req, res) {
-        const atendimentos = await Atendimentos.count();
-        res.status(200).json(atendimentos);
-      },
-      async numeroDePsicologos(req, res) {
-        const psicologos = await Psicologos.count();
-        res.status(200).json(psicologos);
-      },
-      async numeroDePacientes(req, res) {
-        const pacientes = await Pacientes.count();
-        res.status(200).json(pacientes);
-      },
+  async numeroDeAtendimentos(req, res) {
+    const atendimentos = await Atendimentos.count();
+    res.status(200).json(atendimentos);
+  },
+  async numeroDePsicologos(req, res) {
+    const psicologos = await Psicologos.count();
+    res.status(200).json(psicologos);
+  },
+  async numeroDePacientes(req, res) {
+    const pacientes = await Pacientes.count();
+    res.status(200).json(pacientes);
+  },
+  async media(req, res) {
+    const listaDePsicologos = await Psicologos.findAll({
+      include: Atendimentos
+    });
+    let quantAtendimentos = 0
+    listaDePsicologos.forEach(psicologo => {
+      quantAtendimentos += psicologo.Atendimentos.length;
+    });
+    const media = quantAtendimentos / listaDePsicologos.length
 
-      async media(req, res) {
-       
-        const listaDePsicologos = await Psicologos.findAll({
-            include: Atendimentos
-        });
-        let quantAtendimentos = 0
-        listaDePsicologos.forEach(psicologo => {
-            quantAtendimentos += psicologo.Atendimentos.length;
-        });
-        const media = quantAtendimentos/listaDePsicologos.length
- 
-         res.json(media);
-     }
-      
+    res.json(media);
+  }
 }
 module.exports = dashboardController
